4733e6897ba543c4268f2b88a10abbf561d4b871
Jim <jmjmjim1101@gmail.com>
2025-05-09 23:46:28 +0800

    conf: extension setting

--- sidebar/vite.config.ts
+++ sidebar/vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Make sure all projects you build are at this address
+  base: './',
   plugins: [react()],
 })
